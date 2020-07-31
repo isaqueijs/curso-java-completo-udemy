5b52f6a2167c623945e52f6e91461de4157d12da
isaqueijs <isaqueijs@hotmail.com>
2020-07-31 20:50:03 -0300

    Exercício proposto - S10A98

---
+++ Codigos/ExerciicoFixS10A98/src/application/Program.java
@@ -0,0 +1,53 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+
+		int[][] mat = new int[m][n];
+
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
+				mat[i][j] = sc.nextInt();
+			}
+		}
+
+		System.out.println("Enter a number: ");
+		int x = sc.nextInt();
+		position(x, mat);
+
+		sc.close();
+	}
+
+	public static void position(int x, int mat[][]) {
+
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
+				if (mat[i][j] == x) {
+					System.out.println("Position: " + i + "," + j);
+
+					if (j > 0)
+						System.out.println("Left: " + mat[i][j - 1]);
+					if (j < mat[i].length - 1)
+						System.out.println("Right: " + mat[i][j + 1]);
+
+					if (i > 0)
+						System.out.println("Up: " + mat[i - 1][j]);
+					if (i < mat.length - 1)
+						System.out.println("Right: " + mat[i + 1][j]);
+				}
+			}
+		}
+
+	}
+
+}
